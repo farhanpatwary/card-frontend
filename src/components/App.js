8db4af9c8eb95affaf0f6a8f25297666bd0eb553
farhan <patwary.farhan87@gmail.com>
2019-11-21 22:46:44 +0000

    removed tab menu

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cookies from 'universal-cookie';
-import Tab from 'react-bootstrap/Tab'
-import Tabs from 'react-bootstrap/Tabs'
 
 import NavbarComponent from './NavbarComponent'
 import About from './About'
 import SignUpForm from './SignUpForm'
 import LoginForm from './LoginForm'
 import UserSettings from './UserSettings'
-
-import UserPage from './UserPage'
-import AddContact from './AddContact'
-import Contacts from './Contacts'
-
+import TabPage from './TabPage'
 class App extends Component {
   constructor(){
     super()
@@ -27,6 +21,7 @@ class App extends Component {
       current_user: user,
     }
   }
+
   signIn(){
     const cookies = new Cookies();
     var user = cookies.get('user') === undefined ? undefined : cookies.get('user')
@@ -53,17 +48,11 @@ class App extends Component {
           signIn={this.signIn.bind(this)} 
           signOut={this.signOut.bind(this)}
           />
-          <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="justify-content-center">
-            <Tab eventKey="add" title="Add Friend">
-            <AddContact/>
-            </Tab>
-            <Tab eventKey="profile" title="Home">
-              <UserPage/>
-            </Tab>
-            <Tab eventKey="contacts" title="My Contacts">
-              <Contacts/>
-            </Tab>
-          </Tabs>
+          <Route path='/' exact render={(props) => (
+            <TabPage {...props} 
+            signed_in={this.state.signed_in}
+            />
+          )}/>
           <Route path='/about' component={About}/>
           <Route path='/signupform' render={(props) => (
             <SignUpForm {...props} 
@@ -77,7 +66,6 @@ class App extends Component {
             signed_in={this.state.signed_in}
             />
           )}/>
-          <Route path='/users/:id' component={UserPage}/>
           <Route path='/settings' render={(props)=> (
             <UserSettings {...props}
             signOut={this.signOut.bind(this)}
