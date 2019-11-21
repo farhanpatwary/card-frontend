afd8065b2a2970a9b121b3fcb8644f25268e5195
farhan <patwary.farhan87@gmail.com>
2019-11-21 22:49:24 +0000

    added logout button and functionality.

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -3,19 +3,26 @@ import Cookies from 'universal-cookie';
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button';
 class NavbarComponent extends Component {
     constructor(props) {
         super(props);
         this.logOut = this.logOut.bind(this)
+        this.clickHandler = this.clickHandler.bind(this)
+    }
+    clickHandler(e){
+        e.preventDefault()
+        this.logOut()
     }
     logOut(){
         const cookies = new Cookies();
-        cookies.remove('user');
         cookies.remove('token');
+        cookies.remove('user');
+        cookies.remove('personal_email');
+        cookies.remove('work_email');
+        cookies.remove('short');
+
         this.props.signOut()
-    }
-    optionstyle = {
-        'text-align':'center'
     }
 
 
@@ -23,17 +30,17 @@ class NavbarComponent extends Component {
         if(this.props.signed_in === true){
             return (
                 <Nav className="justify-content-center">  
-                    <Nav.Link href="#memes">My Account</Nav.Link>
-                    <Nav.Link href="#memes">Settings</Nav.Link>
-                    <Nav.Link href="#deets">Log Out</Nav.Link>
+                    <Nav.Link href="/me">My Account</Nav.Link>
+                    <Nav.Link href="/settings">Settings</Nav.Link>
+                    <Button variant="secondary" onClick={this.clickHandler}>Log Out</Button>
                 </Nav>
             )
         }
         else{
             return (
                 <Nav className="justify-content-center">
-                    <Nav.Link href="#deets" style={this.optionstyle} >Sign Up</Nav.Link>
-                    <Nav.Link href="#memes" style={this.optionstyle} >Sign In</Nav.Link>
+                    <Nav.Link href="/signupform">Sign Up</Nav.Link>
+                    <Nav.Link href="/login" >Sign In</Nav.Link>
                 </Nav>
             )
         }  
@@ -44,7 +51,7 @@ class NavbarComponent extends Component {
             <Navbar collapseOnSelect expand="lg"  bg="dark" variant="dark">
                 <Nav className="mr-auto">
                 </Nav>
-            <Navbar.Brand href="#home">Card</Navbar.Brand>
+            <Navbar.Brand href="/">Card</Navbar.Brand>
             <Navbar.Toggle/>
             <Nav className="mr-auto">
                 </Nav>
