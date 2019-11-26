6f6b4a0b94946c7219f4caf6438d2ac33e04eb52
farhan <patwary.farhan87@gmail.com>
2019-11-26 21:05:32 +0000

    now shows user contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Accordion from 'react-bootstrap/Accordion'
+import Alert from 'react-bootstrap/Alert'
 
 const Contact = (props) => {
-
     return (
         <Accordion>
             <Card>
                 <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
-                    <Card.Title></Card.Title>
+                    <Card.Title>
+                        {props.data.name}
+                    </Card.Title>
                     <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="1">
                     <Card.Body>
+                        <p>Personal Email: {props.data.personal_email}</p>
+                        <p>Phone Number: {props.data.phone_number !== "" ? props.data.phone_number : "N/A"}</p>
+                        <Alert variant="dark">Short Code: {props.data.short}</Alert>
                     </Card.Body>
                 </Accordion.Collapse>
             </Card>
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
+import Cookies from 'universal-cookie';
 
 import Contact from './Contact'
 
@@ -13,9 +14,21 @@ export class Contacts extends Component {
         };
     }
     componentDidMount(){
-            
+            const cookies = new Cookies();
+            const token = cookies.get('token');
+            fetch('http://localhost:8000/contacts', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(data => data.json())
+            .then(jsondata => this.setState({
+                contacts: jsondata,
+                loaded:true
+            }))
     }
-
 
     render() {
         if(this.state.loaded === false){
@@ -29,7 +42,7 @@ export class Contacts extends Component {
         }      
         else{
             const content = this.state.contacts.map((contact) => 
-                <Contact/>
+                <Contact data={contact} key={contact._id}/>
             )
             return (
                 <Container>
